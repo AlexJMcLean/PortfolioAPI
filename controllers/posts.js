186adb99ce87ec6c333529aa38529ced1a2f0a77
Alex <alexmcl@hotmail.co.uk>
2022-01-11 07:14:42 +0000

    fixes bug that prevents server from accepting NewPost

--- controllers/posts.js
+++ controllers/posts.js
@@ -12,12 +12,12 @@ export const getPosts = async (req, res) => {
 };
 
 export const createPosts = async (req, res) => {
-  const body = req.body();
-  const newPost = new PostBlog(post);
+  const { title, image, snippet, body, date } = req.body;
+  const newPost = new PostBlog({ title, image, snippet, body, date });
   try {
     await newPost.save();
     res.status(201).json(newPost);
   } catch (error) {
-    res.status(404).json({ message: error.message });
+    res.status(409).json({ message: error.message });
   }
 };
--- models/postBlog.js
+++ models/postBlog.js
@@ -2,8 +2,9 @@ import mongoose from "mongoose";
 
 const postSchema = mongoose.Schema({
   title: String,
-  message: String,
-  creator: String,
+  image: String,
+  snippet: String,
+  body: String,
   createdAt: {
     type: Date,
     default: new Date(),
