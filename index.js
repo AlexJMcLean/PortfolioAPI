d7fc06d35a554c71ee7b36422e848ec4dbc0c90b
Alex <alexmcl@hotmail.co.uk>
2022-02-15 07:14:00 +0000

    bug fixes

--- index.js
+++ index.js
@@ -28,27 +28,6 @@ app.get("/", (req, res) => {
   res.send("Hello from Portfolio API");
 });
 
-process
-  .on("SIGTERM", shutdown("SIGTERM"))
-  .on("SIGINT", shutdown("SIGINT"))
-  .on("uncaughtException", shutdown("uncaughtException"));
-
-setInterval(console.log.bind(console, "tick"), 1000);
-http
-  .createServer((req, res) => res.end("hi"))
-  .listen(process.env.PORT || 3000, () => console.log("Listening"));
-
-function shutdown(signal) {
-  return (err) => {
-    console.log(`${signal}...`);
-    if (err) console.error(err.stack || err);
-    setTimeout(() => {
-      console.log("...waited 5s, exiting.");
-      process.exit(err ? 1 : 0);
-    }, 5000).unref();
-  };
-}
-
 mongoose
   .connect(process.env.CONNECTION_URL, {
     useNewUrlParser: true,
